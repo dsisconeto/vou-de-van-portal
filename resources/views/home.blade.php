d6d3d8ae831a45526bd62a826889562edb7df67f
Dejair Magalhaes Sisconeto <dejairsisconeto23@hotmail.com>
2019-07-16 00:01:00 -0300

    colocando falando2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -442,6 +442,19 @@
   })();
 </script>
 -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5d2d3d52bfcb827ab0cbf152/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  })();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
